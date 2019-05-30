668c2b556d1fc8ec30ddcb41fb1e0aebd613f7d4
Arturo Nevarez <nevarez.arturo@uabc.edu.mx>
2019-05-30 13:43:35 -0700

    Se actualizó la sección de pipes
    Se agregaron ejemplos de los siguintes pipes:
    - Slice
    - Decimal
    - Percent
    - Currency
    - JSON
    - Async

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title   = 'pipes';
-  nombre  =  "Arturo Nevarez";
+  title                = 'pipes';
+  nombre:string        =  "Arturo Nevarez";
+  arreglo:number[]     =  [1,2,3,4,5,6,7,8,9,10];
+  PI:number            =  Math.PI;
+  a:number             =  0.234;
+  salario:number       =  1234.5;
+  heroe:any            =  {
+    nombre:"Logan",
+    clave:"Wolverine",
+    direccion: {
+      calle:"Siempre Viva",
+      casa:"Xmen Mansion"
+    }
+  };
+  valorPromesa        =  new Promise((resolve,reject)  =>  {
+    setTimeout(()  =>  {
+      resolve("Llegó la data!");
+    },3500);
+  });
 }
