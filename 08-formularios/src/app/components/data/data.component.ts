091398ca106f115d854b032939174807f9d34a29
Arturo Nevarez <nevarez.arturo@uabc.edu.mx>
2019-06-17 00:04:23 -0700

    Se terminó la sección 8
    - Se agregó el FormArray
    - Se agregaron las validaciones personalizadas
    - Se agregaron las validaciones asíncronas
    - Se subscribio al observable de los valores de un FormControl
    - Se subscribio al observable de los estados de un FormControl

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -33,7 +34,11 @@ export class DataComponent {
       nombre:"",
       apellido:""
     },
-    correo:""
+    correo:"",
+    apodo:"",
+    password1:"",
+    password2:"",
+    pasatiempos:[]
   };
 
   constructor() { 
@@ -44,28 +49,79 @@ export class DataComponent {
                                        Validators.minLength(2)
                                      ]),
         "apellido": new FormControl("",[
-                                        Validators.required
+                                        Validators.required,
+                                        this.noVilla
                                       ])
       }),    
       "correo": new FormControl("",[
                                       Validators.required,
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
-                                   ])
+                                   ]),
+      "apodo": new FormControl("",[Validators.required],this.existeUsuario),      
+      "password1": new FormControl(""),
+      "password2": new FormControl(""),
+      "pasatiempos": new FormArray([])
     });  
+
+    this.forma.controls['password1'].setValidators(Validators.required);
+    this.forma.controls['password2'].setValidators([Validators.required,this.mismatchPassword.bind(this.forma)]);
+    this.forma.controls['apodo'].valueChanges.subscribe(data => console.log(data));
+    this.forma.controls['apodo'].statusChanges.subscribe(data => console.log(data));
+  }
+
+  private noVilla(control:FormControl):{[s:string]:boolean} {
+    if(control.value === "Villa")
+      return {novilla:true}
+    else
+      return null;
+  }
+
+  private mismatchPassword(control:FormControl):{[s:string]:boolean} {
+    let forma:any  =  this;        
+    if(control.value !== forma.controls['password1'].value) 
+        return {mismatch:true}
+    else
+        return null;    
+  }
+
+  public addHobbie():void {
+    (<FormArray>this.forma.controls['pasatiempos']).push(new FormControl("",Validators.required));
+  }
+
+  public existeUsuario(control:FormControl): Promise<any>|Observable<any> {
+    let promesa = new Promise((resolve,reject) => {
+      setTimeout(() => {
+        if(control.value === "perviousnebula")
+          resolve({existe:true});
+        else
+          resolve(null);
+      }, 3000);
+    });
+    return promesa;
+  }
+
+  public removeHobbie(index:number):void {
+    (<FormArray>this.forma.controls['pasatiempos']).removeAt(index);
   }
 
   public enviarForm() {
-    console.log("antes",this.forma.value);
-    this.forma.reset(this.usuario);
-    console.log("reset",this.forma.value);
-    this.forma.setValue({
-      nombreCompleto:{
-        nombre:"",
-        apellido:""
-      },
-      correo:"arturo.nevarez.villa@gmail.com"
-    });
-    console.log("setValue",this.forma);
+    if(this.forma.valid) {
+      console.log("formulario:",this.forma.value);
+      this.forma.reset(this.usuario);
+      console.log("formulario reseteado:",this.forma.value);
+      this.forma.setValue({
+        nombreCompleto:{
+          nombre:"",
+          apellido:""
+        },
+        correo:"arturo.nevarez.villa@gmail.com",
+        apodo:"",
+        password1:"",
+        password2:"",
+        pasatiempos:[]
+      });
+      console.log("Formulario seteado:",this.forma.value);
+    }
   }
 
 }
